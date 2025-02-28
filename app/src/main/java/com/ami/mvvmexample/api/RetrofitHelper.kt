809bcdf4b9930dd7ca0dd9c6680b70878a47710b
amitsaxena383828 <86585612+amitsaxena383828@users.noreply.github.com>
2025-02-28 21:38:56 +0530

    Full URL

--- app/src/main/java/com/ami/mvvmexample/api/RetrofitHelper.kt
+++ app/src/main/java/com/ami/mvvmexample/api/RetrofitHelper.kt
@@ -4,6 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
+    //https://api.nationalize.io/?name=nathaniel
     private const val BASE_URL="https://api.nationalize.io/"
     fun getInstance():Retrofit{
         return Retrofit.Builder()
